6fd84f512b659a44ff0fcb785d2a9e1c38dbdf6f
Admin <Admin@DESKTOP-IH3SL8L>
2024-07-03 22:39:55 +0300

    working convertor fo file end folder

--- AudioConverter/MainWindow.xaml.cs
+++ AudioConverter/MainWindow.xaml.cs
@@ -2,12 +2,16 @@
 using NAudio.Wave;
 using System.Windows;
 using System.IO;
+using NAudio.Vorbis;
+using Ookii.Dialogs.Wpf;
 
 namespace AudioConverter
 {
     public partial class MainWindow : Window
     {
         private string inputFilePath;
+        private string inputFolderPath;
+        private string folderName;
 
         public MainWindow()
         {
@@ -17,7 +21,7 @@ namespace AudioConverter
         private void SelectFileButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Audio Files|*.mp3;*.wav;*.wma;*.aac;*.flac";
+            openFileDialog.Filter = "Audio Files|*.mp3;*.wav;*.wma;*.aac;*.flac;*.ogg;*.m4a";
             if (openFileDialog.ShowDialog() == true)
             {
                 inputFilePath = openFileDialog.FileName;
@@ -25,48 +29,128 @@ namespace AudioConverter
             }
         }
 
+        private void SelectFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new VistaFolderBrowserDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                inputFolderPath = dialog.SelectedPath;
+                folderName = Path.GetFileName(inputFolderPath.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar));
+                StatusLabel.Content = "Selected Folder: " + folderName;
+            }
+        }
+
         private void ConvertButton_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(inputFilePath))
+            if (!string.IsNullOrEmpty(inputFilePath))
             {
-                MessageBox.Show("Please select a file first.");
-                return;
-            }
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "WAV files (*.wav)|*.wav";
+                saveFileDialog.FileName = Path.GetFileNameWithoutExtension(inputFilePath) + ".wav";
 
-            string newFileName = NewFileNameTextBox.Text.Trim();
-            if (string.IsNullOrEmpty(newFileName) || newFileName == "Enter new file name")
-            {
-                MessageBox.Show("Please enter a new file name.");
-                return;
-            }
-
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "WAV files (*.wav)|*.wav";
-            saveFileDialog.FileName = newFileName + ".wav";
-
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                try
+                if (saveFileDialog.ShowDialog() == true)
                 {
                     string outputFilePath = saveFileDialog.FileName;
+                    ConvertFile(inputFilePath, outputFilePath);
+                }
+            }
+            else if (!string.IsNullOrEmpty(inputFolderPath))
+            {
+                var dialog = new VistaFolderBrowserDialog();
+                dialog.SelectedPath = inputFolderPath;
 
-                    using (var reader = new MediaFoundationReader(inputFilePath))
+                if (dialog.ShowDialog() == true)
+                {
+                    string outputFolderPath = dialog.SelectedPath;
+                    ConvertAllFilesInDirectory(inputFolderPath, outputFolderPath);
+                    StatusLabel.Content = "Conversion Completed: " + folderName;
+                    MessageBox.Show("Conversion Completed!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a file or a folder first.");
+            }
+        }
+
+        private void ConvertAllFilesInDirectory(string directoryPath, string outputFolderPath)
+        {
+            var supportedExtensions = new[] { ".mp3", ".m4a", ".ogg", ".wav", ".wma", ".aac", ".flac" };
+            var files = Directory.GetFiles(directoryPath)
+                                 .Where(file => supportedExtensions.Contains(Path.GetExtension(file).ToLower()))
+                                 .ToArray();
+
+            foreach (var file in files)
+            {
+                string outputFilePath = Path.Combine(outputFolderPath, Path.GetFileNameWithoutExtension(file) + ".wav");
+                ConvertFile(file, outputFilePath);
+            }
+        }
+
+        private void ConvertFile(string inputFilePath, string outputFilePath)
+        {
+            try
+            {
+                using (var reader = CreateAudioReader(inputFilePath))
+                {
+                    if (reader == null)
                     {
-                        var newFormat = new WaveFormat(8000, 16, 1); // 8000 Hz, 16 bit, mono
-                        using (var resampler = new MediaFoundationResampler(reader, newFormat))
-                        {
-                            resampler.ResamplerQuality = 60; // 60 is the highest quality
-                            WaveFileWriter.CreateWaveFile(outputFilePath, resampler);
-                        }
+                        throw new InvalidOperationException("Unsupported audio format.");
                     }
 
+                    var newFormat = new WaveFormat(8000, 16, 1); // 8000 Hz, 16 bit, mono
+                    using (var resampler = new MediaFoundationResampler(reader, newFormat))
+                    {
+                        resampler.ResamplerQuality = 60; // 60 is the highest quality
+                        WaveFileWriter.CreateWaveFile(outputFilePath, resampler);
+                    }
+                }
+
+                if (string.IsNullOrEmpty(inputFolderPath))
+                {
                     StatusLabel.Content = "Conversion Completed: " + Path.GetFileName(outputFilePath);
                     MessageBox.Show("Conversion Completed!");
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.Message);
-                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error converting file: " + inputFilePath + "\n" + ex.Message);
+            }
+        }
+
+        private WaveStream CreateAudioReader(string inputFilePath)
+        {
+            string extension = Path.GetExtension(inputFilePath).ToLower();
+
+            switch (extension)
+            {
+                case ".mp3":
+                case ".wma":
+                case ".aac":
+                case ".flac":
+                case ".m4a":
+                    return new MediaFoundationReader(inputFilePath);
+                case ".wav":
+                    return new WaveFileReader(inputFilePath);
+                case ".ogg":
+                    return new VorbisWaveReader(inputFilePath);
+                default:
+                    return null;
+            }
+        }
+
+        private void ConvertAllFilesInDirectory(string directoryPath)
+        {
+            var supportedExtensions = new[] { ".mp3", ".m4a", ".ogg", ".wav", ".wma", ".aac", ".flac" };
+            var files = Directory.GetFiles(directoryPath)
+                                 .Where(file => supportedExtensions.Contains(Path.GetExtension(file).ToLower()))
+                                 .ToArray();
+
+            foreach (var file in files)
+            {
+                string outputFilePath = Path.Combine(directoryPath, Path.GetFileNameWithoutExtension(file) + ".wav");
+                ConvertFile(file, outputFilePath);
             }
         }
 
@@ -87,30 +171,25 @@ namespace AudioConverter
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
             {
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                if (files.Length > 0)
+                string[] droppedItems = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (var item in droppedItems)
                 {
-                    inputFilePath = files[0];
-                    StatusLabel.Content = "Selected: " + Path.GetFileName(inputFilePath);
+                    if (Directory.Exists(item))
+                    {
+                        inputFolderPath = item;
+                        folderName = Path.GetFileName(item.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar));
+                        StatusLabel.Content = "Selected Folder: " + folderName;
+                        inputFilePath = null;
+                    }
+                    else if (File.Exists(item))
+                    {
+                        inputFilePath = item;
+                        StatusLabel.Content = "Selected: " + Path.GetFileName(item);
+                        inputFolderPath = null;
+                    }
                 }
             }
             e.Handled = true;
-        }
-
-        private void RemoveText(object sender, EventArgs e)
-        {
-            if (NewFileNameTextBox.Text == "Enter new file name")
-            {
-                NewFileNameTextBox.Text = "";
-            }
-        }
-
-        private void AddText(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(NewFileNameTextBox.Text))
-            {
-                NewFileNameTextBox.Text = "Enter new file name";
-            }
         }
     }
 }
